70fc1b1653758156fd75947bde61533721038f6e
guudc <65718626+guudc@users.noreply.github.com>
2024-05-08 12:24:37 +0100

    Update controllers.js

--- src/controllers/controllers.js
+++ src/controllers/controllers.js
@@ -1,32 +1,20 @@
-const { exec } = require('child_process');
-
-
-
 //to wrap tokens user
 exports.wrap = (req, res) => {
     //create new wallet address
     try{  
         req = req.query;
         if(req.code && req.issuer){  
-            // Replace 'your-shell-command' with the actual shell command you want to execute.
-            const cmd = 'soroban lab token wrap --network testnet --asset "' + req.code + ':' + req.issuer + '"'
-            exec(cmd, (error, stdout, stderr) => {
-                if (error) {
-                    let msg;
-                    if(error.message.indexOf('already exists')) {
-                        msg = 'exists'
-                    }
-                    else {msg = 'something went wrong'}
-                    res.send(JSON.stringify({status:"error", msg:msg}))
-                }else if (stderr) {
-                    res.send(JSON.stringify({status:"error", msg:'something went wrong'}))
-                }
-                else {
-                    res.send(JSON.stringify({status:true, id:stdout}))
-                }
-            });
+            //make call to the actual server
+           const response = await fetch(`http://195.26.242.45:4000/wrap?code=${req.code}&issuer=${req.issuer}`);
+           if (!response.ok) {
+               //network error
+               res.send(JSON.stringify({status:'error', msg:'something went wrong'}))
+            }
+            const resp = await response.json();
+            if(!resp.status) {return {status: false, msg:'Internal server error'}}
+            else res.send(resp)
         }
         else{res.send(JSON.stringify({status:'error', msg:'fields missing'}))}
     }
-    catch(e){console.log(e);res.send(JSON.stringify({status:'error', msg:'something went wrong'}))}    
+    catch(e){res.send(JSON.stringify({status:'error', msg:'something went wrong'}))}    
 }  
